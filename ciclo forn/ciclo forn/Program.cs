845d79290447286ccd06a193bc2fae02d8e62d82
Alumno <Alumno@PC15>
2019-08-20 09:53:10 -0500

    new

---
+++ ciclo forn/ciclo forn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ciclo_forn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
